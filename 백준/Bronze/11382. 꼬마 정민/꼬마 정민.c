0871ba2a738a60363cc19c3c420a389a7dd6ed27
gongjae <140873812+gongjae@users.noreply.github.com>
2024-06-03 16:27:22 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(void){
+    long long int A;
+    long long int B;
+    long long int C;
+    scanf("%lld %lld %lld",&A,&B,&C);
+    printf("%lld\n",A+B+C);
+
+    return 0;
+}
+
+
