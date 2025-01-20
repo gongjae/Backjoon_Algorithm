6b6a5a0338d63dc7c86b23412a78de381d02b556
gongjae <140873812+gongjae@users.noreply.github.com>
2025-01-20 14:43:30 +0900

    [Silver V] Title: 덩치, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(){
+    int N, result;
+    int x[50] = {0, };
+    int y[50] = {0, };
+
+    scanf("%d",&N);
+
+    for(int i=0; i<N; i++){
+        scanf("%d %d",&x[i], &y[i]);
+    }
+    for(int i=0; i<N; i++) {
+        result = 1;
+        for(int j=0; j<N; j++) {
+            if(x[i] < x[j] && y[i] < y[j]){
+                result++;
+            }
+        }
+        printf("%d ",result);
+    }
+ return 0;
+}
